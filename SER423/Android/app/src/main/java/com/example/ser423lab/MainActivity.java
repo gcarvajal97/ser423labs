6488ff49705e2fb54b0cd17b9c732a29061059c1
Carvajal, Gianni <gianni.carvajal@etrade.com>
2019-11-13 10:02:21 -0600

    Lab 1: Finalizing lab 1 for Android

--- SER423/Android/app/src/main/java/com/example/ser423lab/MainActivity.java
+++ SER423/Android/app/src/main/java/com/example/ser423lab/MainActivity.java
@@ -25,6 +25,25 @@ import java.util.Scanner;
 
 import edu.asu.bsse.gcarvaj3.ser423labapp.PlaceDescription;
 
+/**
+ * Copyright 2019 Gianni Carvajal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Gianni Carvajal mailto:gcarvaj3@asu.edu
+ * @version November 1, 2019
+ */
+
 public class MainActivity extends AppCompatActivity {
     static final String JSONSTR = "{\"address-title\" : \"ASU West Campus\"," +
                 "\"address-street\" : \"13591 N 47th Ave, Phoenix AZ 85051\"," +
